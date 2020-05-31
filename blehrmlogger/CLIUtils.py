617b91a8ec1d978435222e342d64957a46860435
physboe <boeni10@gmail.com>
2020-05-31 20:17:37 +0200

    logging

--- blehrmlogger/CLIUtils.py
+++ blehrmlogger/CLIUtils.py
@@ -12,13 +12,12 @@ def setLoggingStage(debug):
     if debug:
         logging.basicConfig(level=logging.DEBUG)
     else:
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(level=logging.WARNING)
 
 
 def createParser():
     parser = argparse.ArgumentParser(description="Bluetooth heart rate monitor data logger")
     parser.add_argument("-m", metavar='MAC', type=str, help="MAC address of BLE device (default: auto-discovery)")
-    parser.add_argument("-b", action='store_true', help="Check battery level")
     parser.add_argument("-g", metavar='PATH', type=str, help="gatttool path (default: system available)", default="gatttool")
     parser.add_argument("-o", metavar='FILE', type=str, help="Output filename of the database (default: none)")
     parser.add_argument("-H", metavar='HR_HANDLE', type=str, help="Gatttool handle used for HR notifications (default: none)")
