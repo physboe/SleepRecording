66b1dda24424956fe2b0a9b6626245cfcb547ecc
physboe <boeni10@gmail.com>
2020-05-31 20:06:07 +0200

    constants datalayer

--- blehrmlogger/DatabaseLayer.py
+++ blehrmlogger/DatabaseLayer.py
@@ -5,28 +5,35 @@ import logging
 
 class DatabaseLayer(bleservice.RecordingLoggerInterface):
 
+    COMMIT_COUNT = 15
+    SQL_CREATE_RECORDSESSION = "CREATE TABLE IF NOT EXISTS recordsession (recordsession_id INTEGER PRIMARY KEY AUTOINCREMENT, start INTEGER, end INTEGER)"
+    SQL_CREATE_HRM_DATA = "CREATE TABLE IF NOT EXISTS hrm (tstamp INTEGER, hr INTEGER, rr INTEGER, sensor_contact TEXT , fk_recordsession_id INTEGER, FOREIGN KEY(fk_recordsession_id) REFERENCES recordsession(recordsession_id))"
+    SQL_INSERT_RECORDSESSION = "INSERT INTO recordsession (start) VALUES (?)"
+    SQL_UPDATE_RECORDSESSION = "UPDATE recordsession SET end = ? where recordsession_id = ?"
+    SQL_INSERT_HRM_DATA = "INSERT INTO hrm (tstamp, hr, rr, sensor_contact, fk_recordsession_id) VALUES (?, ?, ?, ?, ?)"
+
     def __init__(self, databaseurl):
         self.__databaseurl = databaseurl
         db = self.__connectToDb()
-        db.execute("CREATE TABLE IF NOT EXISTS recordsession (recordsession_id INTEGER PRIMARY KEY AUTOINCREMENT, start INTEGER, end INTEGER)")
-        db.execute("CREATE TABLE IF NOT EXISTS hrm (tstamp INTEGER, hr INTEGER, rr INTEGER, sensor_contact TEXT , fk_recordsession_id INTEGER, FOREIGN KEY(fk_recordsession_id) REFERENCES recordsession(recordsession_id))")
+        db.execute(self.SQL_CREATE_RECORDSESSION)
+        db.execute(self.SQL_CREATE_RECORDSESSION)
         self.__closeDB(db)
 
     def __updateRecordSession(self, recordsession_id, tstamp):
-        cru = self.__db.execute("UPDATE recordsession SET end = ? where recordsession_id = ?", (tstamp,recordsession_id))
+        cru = self.__db.execute(self.SQL_UPDATE_RECORDSESSION, (tstamp,recordsession_id))
         self.__db.commit()
         return cru.lastrowid
 
     def __insertRecordSession(self, tstamp):
-        cru = self.__db.execute("INSERT INTO recordsession (start) VALUES (?)", (tstamp,))
+        cru = self.__db.execute(self.SQL_INSERT_RECORDSESSION, (tstamp,))
         self.__db.commit()
         return cru.lastrowid
 
     def ___insertHrmData(self, recordsession_id, hr, rr, sensor_contact, tstamp):
         logging.info("Commit hrm_data")
-        self.__db.execute("INSERT INTO hrm (tstamp, hr, rr, sensor_contact, fk_recordsession_id) VALUES (?, ?, ?, ?, ?)", (tstamp, hr, rr, sensor_contact, recordsession_id))
+        self.__db.execute(self.SQL_INSERT_HRM_DATA, (tstamp, hr, rr, sensor_contact, recordsession_id))
         self.__counter = self.__counter + 1
-        if self.__counter >= 5:
+        if self.__counter >= self.COMMIT_COUNT:
             logging.info("Commit hrm_data")
             self.__db.commit()
             self.__counter = 0
