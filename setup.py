9d6e8eee947571e936699ac048c057b81ab0e5f8
physboe <boeni10@gmail.com>
2020-05-31 20:55:31 +0200

    packaging

--- setup.py
+++ setup.py
@@ -4,19 +4,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ble-rr-logger-physboe",
+    name="blerrlogger-physboe",
     version="1.0.0",
     author="Andre Boeni",
     author_email="boeni10@gmail.com",
-    description="A little tool to read RR intervalls from BLE Device and save it",
+    description="A little tool to read RR intervalls from BLE Device and " +
+                "save it to a sqlite3 database. " +
+                "You need gatttool to make it work.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/physboe/BLEHeartRateRRLogger.git",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3.7.3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux",
+        "Operating System :: Linux, Raspberry Pi",
     ],
-    python_requires='>=2.7',
+    python_requires='>=3.7.3',
 )
