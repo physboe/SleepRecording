35018275c8669473d12f3c9d419938054305f582
physboe <boeni10@gmail.com>
2020-05-31 17:00:28 +0200

    python 2

--- blehrmlogger/RecordingLoggerInterface.py
+++ blehrmlogger/RecordingLoggerInterface.py
@@ -1,26 +1,26 @@
-import abc
+#import abc
 
 
-class RecordingLoggerInterface(abc.ABC):
+class RecordingLoggerInterface():
 
-    @abc.abstractclassmethod
+#    @abc.abstractclassmethod
     def __init__(self):
         pass
 
-    @abc.abstractclassmethod
+    #@abc.abstractclassmethod
     def saveRecordSession(self, tstamp):
         pass
 
-    @abc.abstractclassmethod
+#    @abc.abstractclassmethod
     def saveHrmData(self, recordSession, hr, rr, tstamp):
         pass
 
-    @abc.abstractclassmethod
+    #@abc.abstractclassmethod
     def close(self):
         pass
 
 
-class RecordSession(abc.ABC):
+class RecordSession():
 
     def __init__(self):
         pass
