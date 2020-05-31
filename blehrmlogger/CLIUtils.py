2474c2fb4bcfb0a574481a8cb8607619689572fa
physboe <boeni10@gmail.com>
2020-05-31 20:32:04 +0200

    test

--- blehrmlogger/CLIUtils.py
+++ blehrmlogger/CLIUtils.py
@@ -5,9 +5,9 @@ import configparser
 
 LOGGING_FORMAT = "%(asctime)s:%(module)s:%(funcName)s:%(levelname)s: %(message)s"
 
+
 def setLoggingStage(debug):
     print("setloggingstage")
-    logging.debug("Debuggng: " + str(debug))
     if debug:
         logging.basicConfig(format=LOGGING_FORMAT, level=logging.DEBUG)
     else:
@@ -30,7 +30,7 @@ def loadConfigParameter(confpath):
     parser = createParser()
 
     if os.path.exists(confpath):
-        logging.debug("Config File found: " + os.path.abspath(confpath))
+#        logging.debug("Config File found: " + os.path.abspath(confpath))
         config = configparser.ConfigParser()
         config.read([confpath])
         config = dict(config.items("config"))
@@ -41,7 +41,7 @@ def loadConfigParameter(confpath):
         err = False
         for key in config.keys():
             if key not in args:
-                logging.error("Configuration file error: invalid key '" + key + "'.")
+    #            logging.error("Configuration file error: invalid key '" + key + "'.")
                 err = True
         if err:
             raise KeyError("Configuration file error'")
