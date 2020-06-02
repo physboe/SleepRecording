a112965eb216a760efb20baade3c16720c600754
Andre <boeni10@gmail.com>
2020-06-02 17:28:50 +0100

    settings

--- logger_rest_api/settings.py
+++ logger_rest_api/settings.py
@@ -1,5 +1,5 @@
 # Flask settings
-FLASK_SERVER_NAME = '192.168.1.180:80'
+FLASK_SERVER_NAME = '192.168.1.180:8888'
 FLASK_DEBUG = True  # Do not use debug mode in production
 FLASK_ENV = 'development'
 
