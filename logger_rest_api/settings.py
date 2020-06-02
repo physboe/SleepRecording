cca5c26936124824bec2fccc767a63bb2c4ac28f
physboe <boeni10@gmail.com>
2020-06-02 18:13:10 +0200

    env

--- logger_rest_api/settings.py
+++ logger_rest_api/settings.py
@@ -1,6 +1,7 @@
 # Flask settings
-FLASK_SERVER_NAME = '0.0.0.0:8888'
+FLASK_SERVER_NAME = '192.168.1.180:80'
 FLASK_DEBUG = True  # Do not use debug mode in production
+FLASK_ENV = 'development'
 
 # Flask-Restplus settings
 RESTPLUS_SWAGGER_UI_DOC_EXPANSION = 'list'
