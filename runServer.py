3ceef89a63ba59f9fa392a007da15a1c8997959e
physboe <boeni10@gmail.com>
2020-06-03 20:35:15 +0200

    bug

--- runServer.py
+++ runServer.py
@@ -2,7 +2,7 @@
 import logging.config
 import os
 import recording_rest_api as webapp
-from recording_rest_api import app as flaskapp
+from configs import webapp as config
 
 logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'configs/logging.conf'))
 logging.config.fileConfig(logging_conf_path)
@@ -11,8 +11,8 @@ log = logging.getLogger(__name__)
 
 def main():
     webapp.initialize_app()
-    log.info(flaskapp.config)
-    log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(flaskapp.config['SERVER_NAME']))
+    log.info(config)
+    log.info('>>>>> Starting development server at http://{}/api/ <<<<<'.format(config.SERVER_NAME))
     flaskapp.run()
 
 if __name__ == "__main__":
