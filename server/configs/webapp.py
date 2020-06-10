c3885cce10d5a124d7bea95e321cb49922d2a5e3
physboe <boeni10@gmail.com>
2020-06-10 12:14:25 +0200

    hostname

--- server/configs/webapp.py
+++ server/configs/webapp.py
@@ -1,5 +1,5 @@
 # Flask settings
-SERVER_NAME = 'raspbonipi4:8888'
+SERVER_NAME = '192.168.1.20:8888'
 DEBUG = False  # Do not use debug mode in production
 ENV = 'development'
 SSL_CONTEXT = 'adhoc'
