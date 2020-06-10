64c52e95077c896b281980680e54ff0a1132b93e
physboe <boeni10@gmail.com>
2020-06-10 19:32:40 +0200

    test

--- server/recording_rest_api/api/models.py
+++ server/recording_rest_api/api/models.py
@@ -3,8 +3,10 @@ from recording_rest_api.api.restplus import api
 
 
 recordingState = api.model('RecordingState', {
-    'running': fields.Boolean(required=True),
-    'tag' : fields.String(required=False)
+    'RecordingState': {
+        'running': fields.Boolean(required=True),
+        'tag': fields.String(required=False)
+    }
 })
 
 pingResponse = api.model('PingReponse', {
