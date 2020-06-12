6e37e030bf491a92605c9b6a96d2b67e1a5bbbd0
physboe <boeni10@gmail.com>
2020-06-12 16:52:18 +0200

    out of package

--- server/wsgi.py
+++ server/wsgi.py
@@ -3,7 +3,7 @@ from recording_rest_api import app
 import os
 import logging.config
 
-logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../configs/logging.conf'))
+logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), 'configs/logging.conf'))
 logging.config.fileConfig(logging_conf_path)
 log = logging.getLogger(__name__)
 
